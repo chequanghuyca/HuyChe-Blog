d75872e917c0a71ebe22719a5770e1cb7b3cdcb6
Huy Chè <chequanghuybtt@gmail.com>
2023-05-30 17:01:49 +0700

    fix

--- src/components/Headers/ProfilePageHeader.js
+++ src/components/Headers/ProfilePageHeader.js
@@ -1,22 +1,4 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.3.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
+import React from 'react';
 
 // reactstrap components
 
@@ -29,31 +11,20 @@ function ProfilePageHeader() {
     if (window.innerWidth < 991) {
       const updateScroll = () => {
         let windowScrollTop = window.pageYOffset / 3;
-        pageHeader.current.style.transform =
-          "translate3d(0," + windowScrollTop + "px,0)";
+        pageHeader.current.style.transform = 'translate3d(0,' + windowScrollTop + 'px,0)';
       };
-      window.addEventListener("scroll", updateScroll);
+      window.addEventListener('scroll', updateScroll);
       return function cleanup() {
-        window.removeEventListener("scroll", updateScroll);
+        window.removeEventListener('scroll', updateScroll);
       };
     }
   });
 
-  return (
-    <>
-      <div
-        style={{
-          backgroundImage:
-            "url(" + require("assets/img/fabio-mangione.jpg") + ")",
-        }}
-        className="page-header page-header-xs"
-        data-parallax={true}
-        ref={pageHeader}
-      >
-        <div className="filter" />
-      </div>
-    </>
-  );
+  return ( <>
+    <div className='page-header page-header-xs' style={{ backgroundImage: 'url(' + require('assets/img/fabio-mangione.jpg') + ')' }} data-parallax={true} ref={pageHeader} >
+      <div className='filter' />
+    </div>
+  </>);
 }
 
 export default ProfilePageHeader;
--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -1,156 +1,74 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.3.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
-// nodejs library that concatenates strings
-import classnames from "classnames";
-// reactstrap components
-import {
-  Button,
-  Collapse,
-  NavbarBrand,
-  Navbar,
-  NavItem,
-  NavLink,
-  Nav,
-  Container,
-} from "reactstrap";
+import React from 'react';
+import classnames from 'classnames';
+import { Button, Collapse, NavbarBrand, Navbar, NavItem, NavLink, Nav, Container } from 'reactstrap';
 
 function IndexNavbar() {
-  const [navbarColor, setNavbarColor] = React.useState("navbar-transparent");
+  const [navbarColor, setNavbarColor] = React.useState('navbar-transparent');
   const [navbarCollapse, setNavbarCollapse] = React.useState(false);
 
   const toggleNavbarCollapse = () => {
     setNavbarCollapse(!navbarCollapse);
-    document.documentElement.classList.toggle("nav-open");
+    document.documentElement.classList.toggle('nav-open');
   };
 
   React.useEffect(() => {
     const updateNavbarColor = () => {
-      if (
-        document.documentElement.scrollTop > 299 ||
-        document.body.scrollTop > 299
-      ) {
-        setNavbarColor("");
-      } else if (
-        document.documentElement.scrollTop < 300 ||
-        document.body.scrollTop < 300
-      ) {
-        setNavbarColor("navbar-transparent");
+      if (document.documentElement.scrollTop > 299 || document.body.scrollTop > 299) {
+        setNavbarColor('');
+      } else if (document.documentElement.scrollTop < 300 || document.body.scrollTop < 300) {
+        setNavbarColor('navbar-transparent');
       }
     };
 
-    window.addEventListener("scroll", updateNavbarColor);
+    window.addEventListener('scroll', updateNavbarColor);
 
     return function cleanup() {
-      window.removeEventListener("scroll", updateNavbarColor);
+      window.removeEventListener('scroll', updateNavbarColor);
     };
   });
+
   return (
-    <Navbar className={classnames("fixed-top", navbarColor)} expand="lg">
+    <Navbar className={classnames('fixed-top', navbarColor)} expand='lg'>
       <Container>
-        <div className="navbar-translate">
-          <NavbarBrand
-            data-placement="bottom"
-            href="/index"
-            target="_blank"
-            title="Coded by Creative Tim"
-          >
-            Paper Kit React
+        <div className='navbar-translate'>
+          <NavbarBrand data-placement='bottom' href='/index' target='_top' title='Home' >
+            <i className='fa fa-home' style={{ fontSize: '35px' }} />
           </NavbarBrand>
-          <button
-            aria-expanded={navbarCollapse}
-            className={classnames("navbar-toggler navbar-toggler", {
-              toggled: navbarCollapse,
-            })}
-            onClick={toggleNavbarCollapse}
-          >
-            <span className="navbar-toggler-bar bar1" />
-            <span className="navbar-toggler-bar bar2" />
-            <span className="navbar-toggler-bar bar3" />
+          <button aria-expanded={navbarCollapse} className={classnames('navbar-toggler navbar-toggler', { toggled: navbarCollapse })} onClick={toggleNavbarCollapse} >
+            <span className='navbar-toggler-bar bar1' />
+            <span className='navbar-toggler-bar bar2' />
+            <span className='navbar-toggler-bar bar3' />
           </button>
         </div>
-        <Collapse
-          className="justify-content-end"
-          navbar
-          isOpen={navbarCollapse}
-        >
-          <Nav navbar>
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="https://twitter.com/CreativeTim?ref=creativetim"
-                target="_blank"
-                title="Follow us on Twitter"
-              >
-                <i className="fa fa-twitter" />
-                <p className="d-lg-none">Twitter</p>
+        <Collapse className='justify-content-end' navbar isOpen={navbarCollapse} >
+          <Nav navbar >
+            <NavItem >
+              <NavLink data-placement='bottom' href='https://zalo.me/0939260508' target='_blank' title='Twitter'>
+                <i className='fa fa-phone-square' style={{ fontSize: '25px' }} />
+                <p className='d-lg-none'> Twitter</p>
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="https://www.facebook.com/CreativeTim?ref=creativetim"
-                target="_blank"
-                title="Like us on Facebook"
-              >
-                <i className="fa fa-facebook-square" />
-                <p className="d-lg-none">Facebook</p>
+              <NavLink data-placement='bottom' href='https://www.facebook.com/huycheeeeee' target='_blank' title='Facebook' >
+                <i className='fa fa-facebook-square' style={{ fontSize: '25px' }} />
+                <p className='d-lg-none'> Facebook</p>
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
-                target="_blank"
-                title="Follow us on Instagram"
-              >
-                <i className="fa fa-instagram" />
-                <p className="d-lg-none">Instagram</p>
+              <NavLink data-placement='bottom' href='https://www.instagram.com/huycheee/' target='_blank' title='Instagram' >
+                <i className='fa fa-instagram' style={{ fontSize: '25px' }} />
+                <p className='d-lg-none'> Instagram</p>
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="https://www.github.com/CreativeTimOfficial/paper-kit-react?ref=creativetim"
-                target="_blank"
-                title="Star on GitHub"
-              >
-                <i className="fa fa-github" />
-                <p className="d-lg-none">GitHub</p>
+              <NavLink data-placement='bottom' href='https://github.com/chequanghuyca' target='_blank' title='GitHub'>
+                <i className='fa fa-github' style={{ fontSize: '25px' }} />
+                <p className='d-lg-none'> GitHub</p>
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink
-                href="https://demos.creative-tim.com/paper-kit-react/#/documentation?ref=pkr-index-navbar"
-                target="_blank"
-              >
-                <i className="nc-icon nc-book-bookmark" /> Documentation
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <Button
-                className="btn-round"
-                color="danger"
-                href="https://www.creative-tim.com/product/paper-kit-pro-react?ref=pkr-index-navbar"
-                target="_blank"
-              >
-                <i className="nc-icon nc-spaceship"></i> Upgrade to Pro
+              <Button className='btn-round' color='danger' href='https://me.momo.vn/qr-page/P2P/huyche' target='_blank' >
+                <i className='fa fa-usd'></i> Ủng hộ Huy
               </Button>
             </NavItem>
           </Nav>
